60263e7fd2fec884d5e6b64006170c5a545562cd
Tarun Kumar <meets2tarun@gmail.com>
2016-10-05 17:49:26 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/Article-one',function(req,res){
+app.get('/article-one',function(req,res){
     res.sendFile('Article one is requested');
 });
 
-app.get('/Article-two',function(req,res){
+app.get('/article-two',function(req,res){
     res.sendFile('Article two is requested');
 });
-app.get('/Article-three',function(req,res){
+app.get('/article-three',function(req,res){
     res.sendFile('Article three is requested');
 });
 

b0aaa921c0a16c6cef17c26d4107999939a55443
Tarun Kumar <meets2tarun@gmail.com>
2016-10-06 18:14:50 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,12 @@
 console.log('UnLoaded!');
 //move the image
 var img = document.getElementById('madi');
+var marginLeft =0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.oneclick = function (){
+    var interval = setInterval(moveRight,100);
     img.style.marginLeft = '100px';
 };
